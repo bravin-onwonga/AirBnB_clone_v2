366fab2543bc2600e2a3754a86d6ca7fec3e1f83
bravin-onwonga <onbravin@gmail.com>
2024-04-17 11:50:02 +0300

    fix:Add privileges on the databases

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,7 +4,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
-IDENTIFIED BY 'hbnb_dev_Pwd#20';
+IDENTIFIED BY 'hbnb_dev_pwd';
 
 GRANT ALL PRIVILEGES
 ON hbnb_dev_db.*

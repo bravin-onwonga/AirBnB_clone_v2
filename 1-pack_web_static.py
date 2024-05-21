7feeae0d5ba2a8935d095d5ad98f11565d4f5ede
bravin-onwonga <onbravin@gmail.com>
2024-05-22 02:18:44 +0300

    chore:Create an archive using fabric

---
+++ 1-pack_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Generates a .tgz archive from the contents of the web_static using do_pack
+"""
+
+import os
+from fabric import task
+from datetime import datetime
+
+@task
+def do_pack(c):
+    """
+    Function that connect to the servers and archives the content
+    """
+
+    time_now = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_" + time_now + ".tgz"
+    res = c.local('tar -czf {} web_static'.format(filename))
+
+    if res.ok:
+        path = os.path.getsize(filename)
+        return(path)
+    else:
+        return None
+
+if __name__ == "__main__":
+    from fabric import Connection
+    c = Connection('localhost')
+    do_pack(c)

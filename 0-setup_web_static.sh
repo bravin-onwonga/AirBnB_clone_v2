bf3b6636cbff7c7360d6fbb5acc662fa78446dfe
bravin-onwonga <onbravin@gmail.com>
2024-05-21 19:13:10 +0300

    fix:Restarting the nginx server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,3 +8,4 @@ echo 'We are live' | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i 's|server_name _;|server_name _;\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}|g' /etc/nginx/sites-enabled/default
+sudo service nginx restart

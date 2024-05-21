527dee2a2c213dfca2752a4732dd94251b25d472
bravin-onwonga <onbravin@gmail.com>
2024-05-21 19:01:11 +0300

    fix:Fixing spelling error enables -> enabled

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,8 +4,7 @@ sudo apt update
 sudo apt install nginx -y
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo touch /data/web_static/releases/test/index.html
 echo 'We are live' | sudo tee /data/web_static/releases/test/index.html
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i 's|server_name _;|server_name _;\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}|g' /etc/nginx/sites-enables/default
+sudo sed -i 's|server_name _;|server_name _;\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}|g' /etc/nginx/sites-enabled/default

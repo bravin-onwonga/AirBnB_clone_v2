9ec75f5fcf1ea315047cc3c32557491bd4585bb8
bravin-onwonga <onbravin@gmail.com>
2024-05-22 13:04:50 +0300

    Route accepting variables

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """
+    Simple route display Hello HBNB
+    """
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Simple route to display HBNB
+    """
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def route_vars(text):
+    """
+    Simple route that accepts a string
+    Returns:
+        string with _ instead of space
+    """
+    text = text.replace('_', ' ')
+    return ("C {}".format(text))
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000, debug=True)

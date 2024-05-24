8b924ad51cc33990c24a5f491b6c11d81aa4a648
bravin-onwonga <onbravin@gmail.com>
2024-05-24 15:41:25 +0300

    fix:Email can't be null

--- models/user.py
+++ models/user.py
@@ -12,7 +12,7 @@ class User(BaseModel, Base):
     if os.environ["HBNB_TYPE_STORAGE"] == 'db':
         __tablename__ = 'users'
 
-        email = Column(String(128))
+        email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)

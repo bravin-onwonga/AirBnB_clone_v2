2481320171831bbbb5de48c83932a2e473a471e2
bravin-onwonga <onbravin@gmail.com>
2024-05-24 19:03:23 +0300

    fix:Fix ForeignKey assignment

--- models/review.py
+++ models/review.py
@@ -10,9 +10,9 @@ class Review(BaseModel, Base):
     if models.my_env == "db":
         __tablename__ = "reviews"
 
-        place_id = Column(String(1024), nullable=False)
-        user_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-        text = Column(String(60), ForeignKey("users.id"), nullable=False)
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
 
     else:
         place_id = ""

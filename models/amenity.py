2bc33ff6f0707729f789b13c2ea29c519f1997dc
bravin-onwonga <onbravin@gmail.com>
2024-05-29 13:01:03 +0300

    fix:Fixing circular importation error

--- models/amenity.py
+++ models/amenity.py
@@ -4,7 +4,6 @@ from sqlalchemy import Column, String
 import models
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -13,7 +12,7 @@ class Amenity(BaseModel, Base):
 
         name = Column(String(128), nullable=False)
         place_amenities = relationship(
-            'Place', secondary=place_amenity, back_populates='amenities')
+            'Place', secondary='place_amenity', back_populates='amenities')
 
     else:
         name = ""

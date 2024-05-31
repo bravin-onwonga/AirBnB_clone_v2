62941315d55cad96c8ab29cd85413930c812b789
bravin-onwonga <onbravin@gmail.com>
2024-05-31 19:07:01 +0300

    Fixing styling errors

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,8 +13,10 @@ env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-    """
-    Function to distributes archived file to the web server
+    """Function to distributes archived file to the web server
+    Parameters:
+       archive_path- path to archive
+    Returns: True if a success; otherwise false
     """
     if not os.path.isfile(archive_path):
         return False

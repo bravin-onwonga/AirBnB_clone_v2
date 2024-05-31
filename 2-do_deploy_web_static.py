a64092de75dda103d9cd3e411c808eddde7290a4
bravin-onwonga <onbravin@gmail.com>
2024-05-31 19:02:41 +0300

    Fixing styling errors

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Distributes archived file to the web server based on the file 1-pack_web_static.py
+Distributes archived file to the web server based on
+the file 1-pack_web_static.py
 """
 
 from fabric import *
@@ -14,13 +15,6 @@ env.user = 'ubuntu'
 def do_deploy(archive_path):
     """
     Function to distributes archived file to the web server
-    Extract and creates a symblic link to the extracted folder
-
-    Parameter:
-        - archive_path: path to archive
-    Returns:
-        - True: success
-        - False: failure
     """
     if not os.path.isfile(archive_path):
         return False
@@ -30,7 +24,8 @@ def do_deploy(archive_path):
         archive = archive_filename.split('.')[0]
         destination = '/data/web_static/releases/{}'.format(archive)
         run('mkdir -p {}'.format(destination))
-        run('tar xf /tmp/{} --directory={}'.format(archive_filename, destination))
+        run('tar xf /tmp/{} --directory={}'.format(
+            archive_filename, destination))
         run('rm /tmp/{}'.format(archive_filename))
         run('mv {}/web_static/* {}'.format(destination, destination))
         run('rm -rf {}/web_static'.format(destination))
